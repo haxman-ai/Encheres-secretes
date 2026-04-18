82303d47f3614577470d7e70174a4eb8f644a4e9
Zem47 <zem47@lenovohaxman.localdomain>
2026-04-18 15:05:03 +0200

    page dacceuille et lien vers le detail

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Repository\ItemRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\HttpCache\ResponseCacheStrategy;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
@@ -13,11 +16,31 @@ final class HomeController extends AbstractController
     public function index(ItemRepository $itemRepository): Response
     {
         $items = $itemRepository->findBy([
-            'status' => 'published'
+            'status' => ['published','closed']
         ]);
 
         return $this->render('home/index.html.twig', [
             'items' => $items,
         ]);
     }
+
+
+    #[Route('/item/{id}',name:'app_item_show')]
+    public function show(int $id, ItemRepository $itemRepository,): Response
+    {  
+        $item = $itemRepository->find($id);
+
+        if(!$item){
+            throw $this->createNotFoundException('objet introuvable.');
+        }
+
+        return $this->render('home/show.html.twig',[
+               
+            'item'=>$item,
+            
+        ]);
+
+
+    }
+
 }
