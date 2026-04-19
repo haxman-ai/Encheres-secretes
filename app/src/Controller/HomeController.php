97b79560d0545c568f332ac779d41034fd279f05
Zem47 <zem47@lenovohaxman.localdomain>
2026-04-19 14:08:22 +0200

    partie 1 projet encheres terminer

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -2,44 +2,50 @@
 
 namespace App\Controller;
 
-use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\ItemRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\HttpCache\ResponseCacheStrategy;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ItemRepository $itemRepository, CategoryRepository $categoryRepository ,): Response
-    {
-       $items = $itemRepository->findPublishedOrClosed();
+    public function index(
+        Request $request,
+        ItemRepository $itemRepository,
+        CategoryRepository $categoryRepository
+    ): Response {
+        $categories = $categoryRepository->findAll();
+
+        $categoryId = $request->query->get('category');
+        $category = null;
+
+        if ($categoryId) {
+            $category = $categoryRepository->find($categoryId);
+            $items = $itemRepository->findByCategory($category);
+        } else {
+            $items = $itemRepository->findPublishedOrClosed();
+        }
 
         return $this->render('home/index.html.twig', [
             'items' => $items,
+            'categories' => $categories,
         ]);
     }
 
-
-    #[Route('/item/{id}',name:'app_item_show')]
-    public function show(int $id, ItemRepository $itemRepository,): Response
-    {  
+    #[Route('/item/{id}', name: 'app_item_show')]
+    public function show(int $id, ItemRepository $itemRepository): Response
+    {
         $item = $itemRepository->find($id);
 
-        if(!$item){
+        if (!$item) {
             throw $this->createNotFoundException('objet introuvable.');
         }
 
-        return $this->render('home/show.html.twig',[
-               
-            'item'=>$item,
-            
+        return $this->render('home/show.html.twig', [
+            'item' => $item,
         ]);
-
-
     }
-
 }
